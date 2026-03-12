4656568cf06370982f13b5fbd951f2407b8c7dc0
djsz123456 <3846603042@qq.com>
2026-03-12 15:37:41 +0800

    Add files via upload

---
+++ 二级Python/答案/PY301-1.py
@@ -0,0 +1,19 @@
+# 以下代码为提示框架
+# 请在...处使用一行或多行代码替换
+# 请在______处使用一行代码替换
+#
+# 注意：提示框架代码可以任意修改，以完成程序功能为准
+
+fi=open("小女孩.txt","r")
+fo = open("PY301-1.txt","w")
+s=fi.read()
+fi.close()
+d = {}
+exclude=',。！？、（）【】<>《》=:+-*— ""…， '
+for i in s:
+    if i not in exclude:
+        d[i]=d.get(i,0)+1
+ls=list(d.items())
+ls.sort(key=lambda x:x[1],reverse=True)
+fo.write("{}:{}".format(ls[0][0],ls[0][1]))
+fo.close()
---
+++ 二级Python/答案/PY301-2.py
@@ -0,0 +1,19 @@
+# 以下代码为提示框架
+# 请在...处使用一行或多行代码替换
+#
+# 注意：提示框架代码可以任意修改，以完成程序功能为准
+
+fi=open("小女孩.txt","r")
+fo = open("PY301-2.txt","w")
+s=fi.read()
+fi.close()
+d = {}
+exclude='\n'
+for i in s:
+    if i not in exclude:
+        d[i]=d.get(i,0)+1
+ls = list(d.items())
+ls.sort(key=lambda x:x[1], reverse=True) # 此行可以按照词频由高到低排序
+for i in range(10):
+    fo.write("{}".format(ls[i][0]))
+fo.close()
